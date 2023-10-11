42be18330305e0bcaa9d0354b10b4a654abb3dbc
Soham-bansal <144805293+Soham-bansal@users.noreply.github.com>
2023-10-11 12:42:45 +0000

    commit

---
+++ .devcontainer/1_js/datatypes.js
@@ -0,0 +1,14 @@
+let a="chutiya";
+let b=`the aaditya is ${a} `;
+console.log(typeof(a));
+console.log(b);
+alert(3+2);
+//number
+//BigInt
+//boolean
+//Undefined
+//string
+//symbol
+//null
+
+//object
---
+++ .devcontainer/1_js/variables.js
@@ -0,0 +1,9 @@
+const accountId="12345";
+let accountemail="bansalsoham92@gmail.com";
+var accountpassword="12451";
+accountcity="jaipur";
+//const can't change
+accountemail="ab.com"
+accountpassword="00000";
+accountcity="ludhiana";
+console.table([accountId,accountemail,accountpassword,accountcity]);
