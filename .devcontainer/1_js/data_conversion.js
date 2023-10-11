766cc972d30d2ebc5132aa7f0336d370f44f5123
Soham-bansal <144805293+Soham-bansal@users.noreply.github.com>
2023-10-11 13:27:53 +0000

    commit

---
+++ .devcontainer/1_js/data_conversion.js
@@ -0,0 +1,15 @@
+
+let score="33abc"
+console.log(typeof (score));
+let number=Number(score);
+console.log(typeof (number));
+console.log(number);
+//true->1 false->0
+//"33"->33
+//"23asb"->Nan
+//number to bool
+let num=0
+let bool=Boolean(num)
+console.log(typeof (bool));
+console.log(bool);
+//
