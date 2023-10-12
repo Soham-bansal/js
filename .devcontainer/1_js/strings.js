7a0246c6c5d548ba1c5321928250ba76d70d58c3
Soham-bansal <144805293+Soham-bansal@users.noreply.github.com>
2023-10-12 10:21:13 +0000

    commit

---
+++ .devcontainer/1_js/strings.js
@@ -0,0 +1,14 @@
+let name="Soham";
+console.log(name.length);
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+
+str="           soham";
+console.log(str.trim());
+
+str="soham";
+console.log(str.substring(0,4));
+dek="soham-jasmine-bansal"
+console.log(dek.split('-'));//convert string to array
+
+
