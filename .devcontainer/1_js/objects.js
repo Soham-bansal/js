a709891d1747b99f0c50b34c9d32fc8a8ad6f6b3
Soham-bansal <144805293+Soham-bansal@users.noreply.github.com>
2023-10-12 17:03:53 +0000

    commit

---
+++ .devcontainer/1_js/objects.js
@@ -0,0 +1,53 @@
+let obj={
+    name:"soham",
+    id:"bansalsoham92@gmail.com",
+    roll:102216052,
+    "name":"soham Bansal",
+    isloggedin:false,
+}
+console.log(obj["id"]);
+console.log(obj.id);
+console.log(obj["name"]);
+
+
+//to add symbol in object
+const mysmb=Symbol("mykey1");
+const objec={
+    name:"soham",
+    id:"bansalsoham92@gmail.com",
+    roll:102216052,
+    [mysmb]:"mykey1"
+}
+console.log(objec);
+console.log(objec["mysmb"]);
+
+//objects decreations
+const ob={
+    coursename:"jsinhindi",
+    courseinstructor:"hitesh",
+    price:"999"
+}
+//decreations
+ const  {courseinstructor:inst}=ob;
+ const {coursename:name}=ob;
+ const  {price:p}=ob;
+//  console.log(courseinstructor);
+//  console.log(coursename);
+//  console.log(price);
+console.log(p);
+console.log(name);
+console.log(inst);
+
+//API
+// {
+//     "name":"soham",
+//     "coursename":"js in hindi",
+//     "price":"free"
+// }
+
+
+
+
+
+
+
