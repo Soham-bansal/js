b0b19e59ea02a0dbe4fb3574f2b31a06a4bf69d2
Soham-bansal <144805293+Soham-bansal@users.noreply.github.com>
2023-10-12 10:51:34 +0000

    commit

---
+++ .devcontainer/1_js/numbersanddata.js
@@ -0,0 +1,23 @@
+const score=400;
+console.log(score);
+const next=new Number(400);
+console.log(next);
+
+console.log(next.toString());
+// console.log(typeof next);
+const ne=400.165416511;
+console.log(ne.toPrecision(5));
+// console.log(ne.);
+
+//maths functions in js
+// console.log(Math.abs(-10));
+// console.log(Math.PI);
+// console.log(Math.atan(0));
+// console.log(Math.ceil(10.165));
+// console.log(Math.round(4.65));
+//random function in math
+console.log(Math.floor(Math.random()*100));
+//values from 10 to 20
+const max=20;
+const min=10;
+console.log(Math.floor((Math.random()*(max-min+1)))+min);
