0e8406b7745c6e37db0a464448f050e572d3a744
Soham-bansal <144805293+Soham-bansal@users.noreply.github.com>
2023-10-12 11:09:15 +0000

    commit

---
+++ .devcontainer/1_js/date and time.js
@@ -0,0 +1,17 @@
+//date and time concept in js
+let mydate=new Date();
+console.log(mydate);
+console.log(mydate.toDateString());//other format
+console.log(mydate.toLocaleDateString());
+console.log(mydate.toISOString());
+console.log(mydate.toLocaleString());
+console.log(mydate.toString());
+
+//
+console.log(mydate.getDate()); //gave date
+console.log(mydate.getDay());//gave day
+console.log(mydate.getFullYear());
+console.log(mydate.getHours());
+// console.log(mydate.);
+
+
